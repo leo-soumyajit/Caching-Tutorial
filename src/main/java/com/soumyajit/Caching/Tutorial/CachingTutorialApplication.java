6ca0ea21e8df213030fbf83db9728fb05f6f4a79
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-03-18 20:38:07 +0530

    2.1 Introduction to Caching

---
+++ src/main/java/com/soumyajit/Caching/Tutorial/CachingTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.soumyajit.Caching.Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soumyajit/Caching/Tutorial/CachingTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumyajit.Caching.Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
